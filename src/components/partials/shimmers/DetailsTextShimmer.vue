f2588927020406491de1f394a02c3116efedfddc
nebstarmalala <nebstartrizzle@gmail.com>
2024-09-27 11:54:48 +0300

    add a details page shimmer text

---
+++ src/components/partials/shimmers/DetailsTextShimmer.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="mt-1">
+      <vue-skeleton-loader
+        :width="120"
+        :height="15"
+      />
+      <vue-skeleton-loader
+        class="mt-1"
+        :width="220"
+        :height="12"
+      />
+    </div>
+  </template>
+  
+  <script>
+  import VueSkeletonLoader from 'skeleton-loader-vue'
+  
+  export default {
+    components: {
+      VueSkeletonLoader,
+    },
+  
+  }
+  </script>
+  
+  <style>
+  
+  </style>
